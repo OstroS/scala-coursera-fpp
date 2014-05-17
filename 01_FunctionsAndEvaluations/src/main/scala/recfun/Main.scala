34cb98a3bac38f4a7bb773f1f85a090e0e8e0b93
OstroS <ostros@gmail.com>
2014-05-17 14:17:56 +0200

    Week 1 assignment / solution scored 10/10

--- 01_FunctionsAndEvaluations/src/main/scala/recfun/Main.scala
+++ 01_FunctionsAndEvaluations/src/main/scala/recfun/Main.scala
@@ -13,16 +13,33 @@ object Main {
 
   /**
    * Exercise 1
+   * c - column from 0
+   * r - row from 0
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0  || r == 0 || r == c ) 1 
+    else pascal(c-1 ,r-1 ) + pascal(c ,r-1 )
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanced(chars: List[Char], open: Int): Boolean = 
+      if (chars.isEmpty) open == 0
+      else if (chars.head == '(') balanced(chars.tail, open + 1)
+      else if (chars.head == ')') open > 0 && balanced(chars.tail, open - 1)
+      else balanced(chars.tail, open)
+    balanced(chars, 0)
+}
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty) 0
+    else if(money.equals(0)) 1
+    else if (money < 0) 0
+    else countChange( money, coins.tail ) + countChange( money - coins.head, coins )
+  }
 }
--- 01_FunctionsAndEvaluations/src/test/scala/recfun/PascalSuite.scala
+++ 01_FunctionsAndEvaluations/src/test/scala/recfun/PascalSuite.scala
@@ -19,4 +19,13 @@ class PascalSuite extends FunSuite {
   test("pascal: col=1,row=3") {
     assert(pascal(1,3) === 3)
   }
+  
+  test("pascal col=2, row=5") {
+    print(pascal(2,5))
+    assert(pascal(2,5) == 10)
+  }
+  
+  test("pascal col=5 row = 23") {
+    assert(pascal(5,23) == 33649)
+  }
 }
